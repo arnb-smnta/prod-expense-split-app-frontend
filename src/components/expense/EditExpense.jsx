23ce4bf7bd2cd6ff6c0bc895f68175f74e2533ad
Arnab Samanta <loveumearnab.2812000@gmail.com>
2024-08-01 19:27:21 +0530

    adding the edit expense

--- src/components/expense/EditExpense.jsx
+++ src/components/expense/EditExpense.jsx
@@ -1,7 +1,263 @@
-import React from "react";
+import { viewExpense, viewExpenseGroupDetails } from "@/api";
+import {
+  AvailableExpenseTypes,
+  AvailablePaymentMethods,
+  requestHandler,
+} from "@/lib/helpers";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { toast } from "../ui/use-toast";
+import { Input } from "../ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
+import { Button } from "../ui/button";
+import { cn } from "@/lib/utils";
 
 const EditExpense = () => {
-  return <div>EditExpense</div>;
+  const { id } = useParams();
+  const [expenseMembersList, setexpenseMembersList] = useState([]);
+  const [date, setDate] = useState();
+  const [isLoading, setisLoading] = useState(true);
+
+  const [participantList, setparticipantsList] = useState([]);
+
+  const [expense, setExpense] = useState({
+    name: "",
+    description: "",
+    amount: "",
+    category: "",
+    expenseDate: "",
+    participants: [],
+    expenseMethod: "",
+    owner: "",
+    groupId: "",
+  });
+
+  const getExpenseDetails = () => {
+    requestHandler(
+      async () => await viewExpense(id),
+      null,
+      (res) => {
+        const {
+          name,
+          description,
+          amount,
+          category,
+          expenseDate,
+          participants,
+          expenseMethod,
+          owner,
+          groupId,
+        } = res.data.payload;
+
+        setExpense({
+          name,
+          description,
+          amount,
+          category,
+          expenseDate,
+          participants,
+          expenseMethod,
+          owner: owner[0],
+          groupId: groupId[0],
+        });
+      },
+      toast
+    );
+  };
+
+  const getGroupDetails = async () => {
+    requestHandler(
+      async () => await viewExpenseGroupDetails(expense.groupId._id),
+      null,
+      (res) => {
+        setparticipantsList(res.data.Group[0].participants);
+      },
+      toast
+    );
+  };
+
+  const handleOnchange = (name) => (e) => {
+    if (name === "owner" || name === "category" || name === "expenseMethod") {
+      setExpense({ ...expense, [name]: e });
+    } else {
+      setExpense({ ...expense, [name]: e.target.value });
+    }
+  };
+
+  useEffect(() => {
+    getExpenseDetails();
+  }, []);
+  useEffect(() => {
+    getGroupDetails();
+  }, [expense]);
+
+  useEffect(() => {
+    if (participantList.length > 0) {
+      const mappedParticiapants = participantList.map((user) => ({
+        value: user._id,
+        label: user.email,
+        icon: undefined,
+      }));
+
+      setexpenseMembersList(mappedParticiapants);
+    }
+  }, [participantList]);
+
+  return (
+    <div className="flex justify-center items-center">
+      <div className="bg-white w-[50%] p-6 mt-8 rounded-2xl">
+        <h1 className="font-bold text-2xl ">Add Expense</h1>
+        <div className="mt-2">
+          <h1 className="font-bold text-xl mb-1">Expense Name</h1>
+          <Input
+            className=""
+            placeholder="Expense Name"
+            value={expense.name}
+            onChange={handleOnchange("name")}
+          />
+        </div>
+
+        <div className="my-2">
+          <h1 className="font-bold text-xl">Expense Description</h1>
+          <Input
+            className=""
+            placeholder="Expense Description"
+            value={expense.description}
+            onChange={handleOnchange("description")}
+          />
+        </div>
+        <div className="flex justify-between my-2">
+          <div>
+            <h1 className="font-bold text-xl">Expense Owner</h1>
+          </div>
+          <div>
+            <Select
+              value={expense.owner._id}
+              onValueChange={handleOnchange("owner")}
+            >
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Expense Owner" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Expense Owner</SelectLabel>
+                  {participantList.map((user) => (
+                    <SelectItem key={user._id} value={user._id}>
+                      {user.username}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+
+        <div className="flex justify-between my-2">
+          <div>
+            <h1 className="font-bold text-xl">Expense Category</h1>
+          </div>{" "}
+          <div>
+            <Select
+              value={expense.category}
+              onValueChange={handleOnchange("category")}
+            >
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Category" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Expense Category</SelectLabel>
+                  {AvailableExpenseTypes.map((Type, index) => (
+                    <SelectItem key={index} value={Type}>
+                      {Type}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+        <div className="flex justify-between my-2">
+          <div>
+            <h1 className="font-bold text-xl">Payment Type</h1>
+          </div>
+          <div>
+            <Select
+              value={expense.expenseMethod}
+              onValueChange={handleOnchange("expenseMethod")}
+            >
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Payment Type" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Payment type</SelectLabel>
+                  {AvailablePaymentMethods.map((Type, index) => (
+                    <SelectItem key={index} value={Type}>
+                      {Type}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+        <div className="date-picker flex justify-between my-2">
+          <div>
+            <h1 className="font-bold text-xl">Expense Date</h1>
+          </div>{" "}
+          <div>
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button
+                  variant={"outline"}
+                  className={cn(
+                    "w-[280px] justify-start text-left font-normal",
+                    !date && "text-muted-foreground"
+                  )}
+                >
+                  <CalendarIcon className="mr-2 h-4 w-4" />
+                  {date ? format(date, "PPP") : <span>Pick a date</span>}
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="w-auto p-0">
+                <Calendar
+                  mode="single"
+                  selected={expense.expenseDate}
+                  onSelect={setDate}
+                  initialFocus
+                />
+              </PopoverContent>
+            </Popover>
+          </div>
+        </div>
+
+        <div className="flex justify-between">
+          <Link to={`/dashboard/groups/view/${expense.groupId._id}`}>
+            <Button variant="destructive"> Cancel</Button>
+          </Link>
+
+          <Button className="bg-blue-700 text-white ">Edit Expense</Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default EditExpense;
